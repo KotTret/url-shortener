3736e8fc6019ab9d1726e9dc9317e1a82960e531
Kot.Tret <kot.tret@yandex.ru>
2023-11-19 20:15:16 +0300

    first commit

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	// TODO: init config: cleaneny
+
+	// TODO: init logger: slog
+
+	// TODO: init storage: sqlite
+
+	// TODO: init router: chi
+
+	// TODO: run rserver
+
+}
